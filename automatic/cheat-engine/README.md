a3824515a912e136443248abd840eccb99baecfd
criography <reg@criography.com>
2015-06-23 00:44:41 +0100

    ! cheat-engine: worked on the uninstaller + cheat-engine: added reg-diffs to a package for moderation purposes.

--- automatic/cheat-engine/README.md
+++ automatic/cheat-engine/README.md
@@ -1,2 +1,5 @@
-alternate non-install URL: 
+added VM reg-difs for testing purposes in ./registry-diffs
+
+
+To consider an alternate non-installer URL (not easy to Ketarin though): 
 http://cheatengine.org/download/cheatengine64_NoSetup.rar
--- automatic/cheat-engine/tools/chocolateyUninstall.ps1
+++ automatic/cheat-engine/tools/chocolateyUninstall.ps1
@@ -1,5 +1,5 @@
 $packageName = 'cheat-engine'
-$programName = 'Cheat Engine'
+$programName = 'Cheat Engine {{PackageVersion}}'
 $installerType = 'EXE'
 $silentArgs = '/VERYSILENT /NORESTART'
 
@@ -8,12 +8,25 @@ $silentArgs = '/VERYSILENT /NORESTART'
 try {
 
   # establish all possible locations for uninstaller to be stored
-  $local_key = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*'
-  $machine_key32 = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*'
-  $machine_key64 = 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*'
+  $local_key = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall'
+  $machine_key32 = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall'
+  $machine_key64 = 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall'
+  $reg_locations = @()
+
+  if(Test-Path "$local_key"){
+    $reg_locations += "$local_key\*"
+  }
+
+  if(Test-Path "$machine_key32"){
+    $reg_locations += "$machine_key32\*"
+  }
+
+  if( ((Get-WmiObject Win32_Processor).AddressWidth -eq 64) -and (Test-Path "$machine_key64") ) {
+    $reg_locations += "$machine_key64\*"
+  }
 
   # find the actual uninstaller path
-  $uninstaller = Get-ItemProperty -Path @($local_key, $machine_key32, $machine_key64) | ?{ $_.DisplayName -match $programName }
+  $uninstaller = Get-ItemProperty -Path $reg_locations | ?{ $_.DisplayName -match $programName }
 
   # uninstall package
   Uninstall-ChocolateyPackage $packageName $installerType $silentArgs $uninstaller.UninstallString
