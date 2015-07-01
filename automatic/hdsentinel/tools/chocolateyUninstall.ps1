d72592a0f2eeb990c4e54db285aa672adcd778f8
criography <reg@criography.com>
2015-07-02 00:02:40 +0100

    + HDSentinel

---
+++ automatic/hdsentinel/tools/chocolateyUninstall.ps1
@@ -0,0 +1,18 @@
+﻿. (Join-Path (Split-Path -parent $MyInvocation.MyCommand.Definition) 'helpers\helpers.ps1')
+
+$packageTitle   = "Hard Disk Sentinel"
+$trySilent      = 1;
+$silentArgs     = "/sp- /verysilent /norestart"
+
+
+
+try {
+
+    AutoUninstall `
+        -programName    $packageTitle `
+        -trySilent      $trySilent `
+        -silentargs     $silentArgs
+
+} catch {
+    throw $_.Exception
+}
