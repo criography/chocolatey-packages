fb564105b1dddbd36d30c563523792d22f433d57
criography <reg@criography.com>
2015-07-01 23:34:58 +0100

    general cleanup

--- automatic/garmin-express/tools/chocolateyUninstall.ps1
+++ automatic/garmin-express/tools/chocolateyUninstall.ps1
@@ -1,34 +1,13 @@
 . (Join-Path (Split-Path -parent $MyInvocation.MyCommand.Definition) 'helpers\helpers.ps1')
 
-$packageName    = "garmin-express"
-$packageTitle   = 'Garmin Express'
-$installerType  = "EXE"
-$silentArgs     = ''
-
 
 try {
 
-  # find uninstall string in registry
-  $uninstallString = GetUninstallString $packageTitle 1
-
-  if( $uninstallString -is [System.Array] ){
-    $uninstallString = $uninstallString | ?{ $_ -NotLike "MsiExec.exe*" }
-  }
-
-  $silentArgs      = $uninstallString -replace '^\".*?\"\s+(.*?)$', '$1'
-  $uninstallString = $uninstallString | %{  $_  -replace '\"\s+.*?$', '"' }
-
-
-  # attempt uninstall
-  Uninstall-ChocolateyPackage $packageName $installerType $($silentArgs) $($uninstallString)
-
-  # profit
-  write-host "$packageTitle uninstalled successfully"
-
+    AutoUninstall `
+        -programName  'Garmin Express' `
+        -skipMsi      1 `
+        -trySilent    1
 
 } catch {
-
-  #cry
-  throw $_.Exception.Message
-
+    throw $_.Exception
 }
