0a10757ac2db204668532b36fafdd90a595630e8
criography <reg@criography.com>
2015-07-04 23:49:49 +0100

    added bitmeter]

---
+++ automatic/bitmeter2/tools/chocolateyUninstall.ps1
@@ -0,0 +1,16 @@
+﻿. (Join-Path (Split-Path -parent $MyInvocation.MyCommand.Definition) 'helpers\helpers.ps1')
+
+$packageTitle   = "BitMeter"
+$silentArgs     = "/S /NCRC"
+
+
+
+try {
+
+    AutoUninstall `
+        -programName    $packageTitle `
+        -silentargs     $silentArgs
+
+} catch {
+    throw $_.Exception
+}
