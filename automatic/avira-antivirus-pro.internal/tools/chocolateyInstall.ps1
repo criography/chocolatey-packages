d78ed94629dd337ef818dc8e09ebcd58e4daca97
criography <reg@criography.com>
2015-08-15 19:02:14 +0100

    updated few internal packages

---
+++ automatic/avira-antivirus-pro.internal/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+$packageName    = '{{PackageName}}'
+$installerType  = 'EXE'
+$url            = '{{DownloadUrl}}'
+$silentArgs     = ''
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"
