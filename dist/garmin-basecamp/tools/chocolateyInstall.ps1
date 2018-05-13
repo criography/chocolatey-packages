0e761162bb53b82212c6091faf466c453fbb87c2
marek <marek@nudgecrm.com>
2018-05-13 18:48:22 +0100

    including dist

---
+++ dist/garmin-basecamp/tools/chocolateyInstall.ps1
@@ -0,0 +1,48 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$packageName    = "garmin-basecamp"
+$installerType  = "msi"
+$packageVersion = "4.6.2"
+$url            = "http://download.garmin.com/software/BaseCamp_462.exe"
+$silentArgs     = "/quiet"
+$checksum       = "005eb0c5788397147ca962163b23c163f4460d14711f73fd22ea08eef75e2e6f"
+$checksumType   = "sha256"
+$validExitCodes = @(0)
+
+
+
+#extract filename from source URL
+$filename       = $url.Substring($url.LastIndexOf("/") + 1)
+
+$tmpDir         = Join-Path $env:TEMP "$packageName"
+$extractedDir   = Join-Path $tmpDir "extracted-msi"
+
+# full path to local copy of downloaded zip file
+$pathToZip      = ($tmpDir,$filename -join "\")
+
+
+
+# download zip package
+Get-ChocolateyWebFile  `
+    -PackageName $packageName `
+    -FileFullPath $pathToZip `
+    -Url $url `
+    -Checksum $checksum `
+    -ChecksumType $checksumType
+
+
+
+# extract it
+Get-ChocolateyUnzip `
+  -FileFullPath $pathToZip `
+  -Destination $extractedDir
+
+
+
+# install package
+Install-ChocolateyInstallPackage `
+  -PackageName $packageName `
+  -FileType $installerType `
+  -SilentArgs $silentArgs `
+  -ValidExitCodes $validExitCodes `
+  -File (Join-Path $extractedDir "BCMain.msi")
---
+++ dist/xyplorer/tools/chocolateyInstall.ps1
@@ -0,0 +1,43 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$packageName    = "xyplorer"
+$installerType  = "EXE"
+$packageVersion = "18.90.0100"
+$url            = "https://www.xyplorer.com/download/xyplorer_full.zip"
+$silentArgs     = "/S"
+$validExitCodes = @(0)
+$checksum       = "249d3cf37c0d466eade5b18ea95fae9c91f78019a21a991e73cdd2e6c6cc675c"
+$checksumType   = "sha256"
+
+
+#extract filename from source URL
+$filename       = $url.Substring($url.LastIndexOf("/") + 1)
+
+
+#establish temp folder path
+$tempPath       = $env:temp,$packageName,$packageVersion -join "\"
+
+
+#establish full path to local copy of downloaded zip file
+$pathToZip      = ($tempPath,$filename -join "\")
+
+
+# download zip package
+Get-ChocolateyWebFile  `
+    -PackageName $packageName `
+    -FileFullPath $pathToZip `
+    -Url $url `
+    -Checksum $checksum `
+    -ChecksumType $checksumType
+
+
+# extract it
+Get-ChocolateyUnzip $pathToZip $tempPath
+
+
+# establish path to extracted installer (exe)
+$pathToExe = Join-Path $tempPath (get-childitem $tempPath | where {$_.extension -eq ".exe"}).Name
+
+
+# install package
+Install-ChocolateyInstallPackage "$packageName" "$installerType" "$silentArgs" "$pathToExe" -validExitCodes $validExitCodes
